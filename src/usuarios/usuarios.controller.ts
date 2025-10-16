fdc844f03038546814558f98fbedd30d022924d8
nftencuestas123 <nftencuestas@gmail.com>
2025-10-16 15:02:55 -0300

    Update usuarios.controller.ts

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dtos/create-usuario.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Controller('usuarios')
 export class UsuariosController {
@@ -19,5 +21,11 @@ export class UsuariosController {
   @Get()
   async listar() {
     return this.usuariosService.listar();
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('solo-admin')
+  async getSoloAdmin() {
+    return { mensaje: 'Solo admins acceden' };
   }
 }
