cd5f3db527f50e60ce9932804d2949ac01b00f7d
nftencuestas123 <nftencuestas@gmail.com>
2025-10-17 08:46:26 -0300

    fix: imports y tipado para deploy exitoso

--- src/productos.controller.ts
+++ src/productos.controller.ts
@@ -1,18 +1,15 @@
 import { Controller, Post, UploadedFile, UseInterceptors, Patch, Body, Param } from '@nestjs/common';
-import { ProductosService } from './productos.service';
+import { ProductosService } from './productos/productos.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { EditStockDto } from './dtos/edit-stock.dto';
-
+import { EditStockDto } from './productos/dtos/edit-stock.dto';
 @Controller('productos')
 export class ProductosController {
   constructor(private readonly productosService: ProductosService) {}
-
   @Post('subida-masiva')
   @UseInterceptors(FileInterceptor('archivo'))
-  async subidaMasiva(@UploadedFile() archivo, @Body('usarIA') usarIA: boolean) {
+  async subidaMasiva(@UploadedFile() archivo: Express.Multer.File, @Body('usarIA') usarIA: boolean) {
     return this.productosService.subidaMasiva(archivo, usarIA);
   }
-
   @Patch('editar-stock/:id')
   async editarStock(@Param('id') id: string, @Body() dto: EditStockDto) {
     return this.productosService.editarStock(id, dto.nuevo_stock);
