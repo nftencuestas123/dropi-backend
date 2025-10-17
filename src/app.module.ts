02ccd117d5da4e5e8f76b35c92d232ac605b0af2
nftencuestas123 <nftencuestas@gmail.com>
2025-10-16 22:26:36 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+
+import { AuthModule } from './auth';
+import { UsuariosModule } from './usuarios';
 import { ProductosModule } from './productos';
+import { CrmModule } from './crm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    AuthModule,
+    UsuariosModule,
     ProductosModule,
-    // aquí agregarás el resto de tus módulos: crm, usuarios, logística, etc.
-  ]
+    CrmModule,
+    // Agrega aquí más módulos si introduces nuevas funciones (ejemplo: NotificacionesModule)
+  ],
+  // providers: [], // Si tienes servicios globales o interceptores, agrégalos aquí
 })
 export class AppModule {}
