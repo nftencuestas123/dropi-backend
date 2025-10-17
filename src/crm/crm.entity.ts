01638311edb706e58f77389635646ad02b22d5ca
nftencuestas123 <nftencuestas@gmail.com>
2025-10-17 00:18:58 -0300

    Update crm.entity.ts

--- src/crm/crm.entity.ts
+++ src/crm/crm.entity.ts
@@ -1,22 +1,7 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-
-@Entity('clientes_crm')
 export class ClienteCrm {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
-  @Column()
-  nombre: string;
-
-  @Column()
-  email: string;
-
-  @Column()
-  telefono: string;
-
-  @Column()
-  empresa: string;
-
-  @Column({ type: 'jsonb', nullable: true })
-  datos_extra: any;
+  id: string = '';
+  nombre: string = '';
+  email: string = '';
+  telefono: string = '';
+  empresa: string = '';
 }
