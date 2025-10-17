cd32df7a357dbdb6b79c57af44f808456b9c27df
nftencuestas123 <nftencuestas@gmail.com>
2025-10-17 19:54:33 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { ProductosModule } from './productos';
 import { CrmModule } from './crm';
 import { ConfigModule } from '@nestjs/config';
 
+// Importa el controlador nuevo
+import { ImageUploadController } from './image-upload/image-upload.controller';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,6 +20,10 @@ import { ConfigModule } from '@nestjs/config';
     ProductosModule,
     CrmModule,
     // Agrega aquí más módulos si introduces nuevas funciones (ejemplo: NotificacionesModule)
+  ],
+  controllers: [
+    ImageUploadController,
+    // Si usabas más controladores, agrégalos aquí
   ],
   // providers: [], // Si tienes servicios globales o interceptores, agrégalos aquí
 })
