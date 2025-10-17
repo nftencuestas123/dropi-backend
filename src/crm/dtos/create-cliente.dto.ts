c2cb43a90420531f4c2cc24548f34b59fb6275f1
nftencuestas123 <nftencuestas@gmail.com>
2025-10-17 00:19:31 -0300

    Update create-cliente.dto.ts

--- src/crm/dtos/create-cliente.dto.ts
+++ src/crm/dtos/create-cliente.dto.ts
@@ -1,15 +1,6 @@
-import { IsString, IsEmail } from 'class-validator';
-
 export class CreateClienteDto {
-  @IsString()
-  nombre: string;
-
-  @IsEmail()
-  email: string;
-
-  @IsString()
-  telefono: string;
-
-  @IsString()
-  empresa: string;
+  nombre: string = '';
+  email: string = '';
+  telefono: string = '';
+  empresa: string = '';
 }
