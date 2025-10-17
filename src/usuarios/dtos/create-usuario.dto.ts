8ccf2b6035034792b1433861ff38d968e5988581
nftencuestas123 <nftencuestas@gmail.com>
2025-10-17 00:20:56 -0300

    Update create-usuario.dto.ts

--- src/usuarios/dtos/create-usuario.dto.ts
+++ src/usuarios/dtos/create-usuario.dto.ts
@@ -1,13 +1,5 @@
-import { IsString, IsEmail, MinLength } from 'class-validator';
-
 export class CreateUsuarioDto {
-  @IsString()
-  nombre: string;
-
-  @IsEmail()
-  email: string;
-
-  @IsString()
-  @MinLength(6)
-  password: string;
+  nombre: string = '';
+  email: string = '';
+  password: string = '';
 }
