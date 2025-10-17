4ff1125a16d44757af32104a1f6e2e445dbceef2
nftencuestas123 <nftencuestas@gmail.com>
2025-10-16 22:47:25 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  // Configuración Swagger (OpenAPI)
+  const config = new DocumentBuilder()
+    .setTitle('DROPi Backend API')
+    .setDescription('Documentación automática de endpoints para Dropi Backend (empresarial y segura).')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
